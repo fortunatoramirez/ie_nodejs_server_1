a83f7b86b51c6d9fc7126b3c4aaa6db4c8c2d543
Fortunato <33538533+fortunatoramirez@users.noreply.github.com>
2022-04-28 15:49:48 -0700

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,13 @@
+//var socket = io.connect('localhost:5001', {'forceNew':true});
+
+/*
+socket.on('desde_servidor', function(data){
+    //console.log(data);
+    //data_JSON = JSON.parse(data)
+    
+    document.getElementById("my_div").innerHTML = data;
+
+
+});
+*/
+
